eb06e3f604fb113ad043268539652b623e09b397
Victor-Hugo-F <victor.hugo.freir@gmail.com>
2025-02-20 17:56:00 -0300

    feat: add product details page

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { FormatCurrency } from "@/helpers/format-currency";
+
 interface ProductsProps {
     products: Product[]
 }
@@ -22,10 +24,7 @@ const Products = ({products} : ProductsProps) => {
                         </p>
                         
                         <p className="pt-3 text-sm font-semibold">
-                        {new Intl.NumberFormat("pt-BR" ,{
-                             style: 'currency',
-                            currency: 'BRL'
-                        }).format(product.price)}
+                        {FormatCurrency(product.price)}
                         </p>
                     </div>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 const HomePage = () => {
-  return ( <h1>hello word</h1> );
-}
- 
-export default HomePage;
+    return <div>
+        <h1>hello word!</h1>
+        <h2>URL: http://localhost:3000/fsw-donalds</h2>
+    </div>
+  };
+  
+  export default HomePage;
