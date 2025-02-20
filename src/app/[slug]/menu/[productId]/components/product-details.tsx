d46a8fc382dd5f7ee5063a60702fdb1d9a4b69b9
Victor-Hugo-F <victor.hugo.freir@gmail.com>
2025-02-20 18:48:32 -0300

    feat add scroll to products details page

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { FormatCurrency } from "@/helpers/format-currency";
 
 interface ProductDetailsProps {
@@ -26,9 +27,9 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
         setQuantity((prev) => prev + 1 );
     };
     return ( 
-        <div className="relative z-50 rounded-t-3xl py-5 mt-[-1.5rem] p-5 flex-auto flex flex-col">
+        <div className="relative z-50 rounded-t-3xl py-5 mt-[-1.5rem] p-5 flex-auto flex flex-col overflow-hidden">
           
-            <div className="flex-auto ">
+            <div className="flex-auto overflow-hidden ">
                 <div className="flex items-center gap-1.5">
                     <Image src={product.restaurant.avatarImageUrl} alt={product.restaurant.name} width={16} height={16} className="rounded-full" />
                     <p className="text-xs text-muted-foreground">{product.restaurant.name}</p>
@@ -36,7 +37,7 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
 
                 <h2 className="mt-1 text-xl font-semibold ">{product.name}</h2>
 
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between mt-3">
                 <h3 className="text-xl font-semibold">
                 {FormatCurrency(product.price)}
                 </h3>
@@ -53,6 +54,7 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
                 </div>
                 </div>
 
+                <ScrollArea className="h-full ">
                 <div className="mt-6 space-y-3 ">
                     <h4 className="font-semibold ">
                         Sobre
@@ -69,13 +71,15 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
                         Ingredientes
                     </h4>
                     </div>
-                    <p className="text-sm text-muted-foreground">
-                         {product.ingredients} 
-                    </p>
+                    <ul className="list-disc px-5 text-sm text-muted-foreground">
+                        {product.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
+                    </ul>
                 </div>
+                </ScrollArea>
+
             </div>
 
-                <Button className="rounded-full w-full mt-6">
+                <Button className="rounded-full w-full ">
                     Adicione à sacola
                 </Button>
 
