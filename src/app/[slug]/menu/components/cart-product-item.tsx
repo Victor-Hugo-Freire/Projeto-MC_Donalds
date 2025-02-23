b04edb476a3b2eac52afa829053a3f61d0805558
Victor-Hugo-F <victor.hugo.freir@gmail.com>
2025-02-23 18:46:52 -0300

    feat: ensure cart summary is shown on restaraunt page

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,51 +1,72 @@
-import { ChevronLeftIcon,ChevronRightIcon, TrashIcon } from "lucide-react";
+"use client";
+
+import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
 import { useContext } from "react";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { FormatCurrency } from "@/helpers/format-currency";
 
 import { CartContext, CartProduct } from "../contexts/cart";
 
-interface CartProductItemProps {
-    product: CartProduct
+interface CartItemProps {
+  product: CartProduct;
 }
 
-const CartProductItem = ({product} : CartProductItemProps) => {
-    const {decreaseProductQuantity , increaseProductQuantity, removeProduct} = useContext(CartContext)
-    return ( 
-        <div className="flex items-center justify-between">
-
-     <div className="flex items-center gap-3">
-        <div className="relative h-20 w-20 bg-gray-100 rounded-xl ">
-            <Image src={product.imageUrl} alt={product.name} fill/>
-        </div>
-
-        <div className="space-y-1">
-            <p className="text-xs max-w-[90%] truncate text-ellipsis">{product.name}</p>
-            <p className="text-sm font-semibold">{FormatCurrency(product.price)}</p>
-
+const CartProductItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity, increaseProductQuantity, removeProduct } =
+    useContext(CartContext);
+  return (
+    <ScrollArea className="w-full">
+      <div className="flex items-center justify-between">
+        {/* ESQUERDA */}
+        <div className="flex items-center gap-3">
+          <div className="relative h-20 w-20 rounded-xl bg-gray-100">
+            <Image src={product.imageUrl} alt={product.name} fill />
+          </div>
+          <div className="space-y-1">
+            <p className="max-w-[90%] truncate text-ellipsis text-xs">
+              {product.name}
+            </p>
+            <p className="text-sm font-semibold">
+              {FormatCurrency(product.price)}
+            </p>
+            {/* QUANTIDADE */}
             <div className="flex items-center gap-1 text-center">
-                <Button className="w-7 h-7 rounded-lg" variant="outline" onClick={() => decreaseProductQuantity(product.id)}>
-                    <ChevronLeftIcon  />
-                </Button>
+              <Button
+                className="h-7 w-7 rounded-lg"
+                variant="outline"
+                onClick={() => decreaseProductQuantity(product.id)}
+              >
+                <ChevronLeftIcon />
+              </Button>
+              <p className="w-7 text-xs">{product.quantity}</p>
+              <Button
+                className="h-7 w-7 rounded-lg"
+                variant="destructive"
+                onClick={() => increaseProductQuantity(product.id)}
+              >
+                <ChevronRightIcon />
+              </Button>
 
-                <p className="text-xs  w-6 ">{product.quantity}</p>
-
-                <Button className="w-7 h-7 rounded-lg" variant="destructive" onClick={() => increaseProductQuantity(product.id)}>
-                    <ChevronRightIcon />
+              {/* BOTÃO DE DELETAR */}
+              <div className="p-5">
+                <Button
+                  className="h-7 w-7 rounded-lg"
+                  variant="outline"
+                  onClick={() => removeProduct(product.id)}
+                >
+                  <TrashIcon />
                 </Button>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
+      <ScrollBar orientation="vertical" />
+    </ScrollArea>
+  );
+};
 
-     </div>
-
-     <Button className="h-7 w-7 rounded-lg " variant="outline" onClick={() => removeProduct(product.id)}>
-        <TrashIcon  />
-     </Button>
-
-        </div>
-     );
-}
- 
 export default CartProductItem;
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -12,6 +12,7 @@ export interface ICartContext {
   isOpen: boolean;
   products: CartProduct[];
   total: number;
+  totalQuantity: number;
   togleCart: () => void;
   addProduct: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
@@ -22,6 +23,7 @@ export interface ICartContext {
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
   total: 0,
+  totalQuantity: 0,
   products: [],
   togleCart: () => {},
   addProduct: () => {},
@@ -35,7 +37,11 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const total = products.reduce((acc, product) => {
-    return (acc = product.price * product.quantity);
+    return acc + product.price * product.quantity;
+  }, 0);
+
+  const totalQuantity = products.reduce((acc, product) => {
+    return acc + product.quantity;
   }, 0);
 
   const togleCart = () => {
@@ -104,6 +110,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         increaseProductQuantity,
         removeProduct,
         total,
+        totalQuantity,
       }}
     >
       {children}
