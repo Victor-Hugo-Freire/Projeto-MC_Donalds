cd4bc5f2ea50ebd742cfb2945f7fc17954f2ef47
Victor-Hugo-F <victor.hugo.freir@gmail.com>
2025-02-23 23:23:41 -0300

    feat: changing URLs from HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 const HomePage = () => {
-    return <div>
-        <h1>hello word!</h1>
-        <h2>URL: http://localhost:3000/fsw-donalds</h2>
+  return (
+    <div>
+      <h1>hello word!</h1>
+      <h2>
+        URL:
+        https://projeto-mc-donalds-git-main-victor-hugo-projects-projects.vercel.app/fsw-donalds
+      </h2>
+      <h2>OU</h2>
+      <h2>URL: https://projeto-mc-donalds.vercel.app/fsw-donalds</h2>
     </div>
-  };
-  
-  export default HomePage;
+  );
+};
+
+export default HomePage;
